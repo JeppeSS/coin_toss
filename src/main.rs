af28718f9b078ae876bbaad680ffe2c499f85dfb
Jeppe Skov <jsskov92@gmail.com>
2022-10-19 21:46:50 +0200

    Game logic refac

--- src/main.rs
+++ src/main.rs
@@ -1,26 +1,9 @@
 use std::io;
 
-#[derive(PartialEq, Eq)]
+#[derive(Debug, PartialEq, Eq)]
 enum CoinFace {
     Heads,
     Tails,
-}
-
-struct GameState {
-    tossed_coin_side: CoinFace,
-}
-
-impl GameState {
-    fn new() -> GameState {
-        let coin_flip = match rand::random::<bool>() {
-            true => CoinFace::Heads,
-            false => CoinFace::Tails,
-        };
-
-        GameState {
-            tossed_coin_side: coin_flip,
-        }
-    }
 }
 
 fn main() {
@@ -28,39 +11,56 @@ fn main() {
     println!("Welcome to the coin toss guessing game! The rules are simple, guess which side of the coin is showing when it lands - Heads or tails!");
 
     println!(" ");
-    println!("Guess a side!");
-
-    let game = GameState::new();
-
-    println!("Enter one of the following options:");
-    println!(" ");
-    println!("For Heads:");
-    println!("  H, Head, Heads");
-    println!(" ");
-    println!("For Tails:");
-    println!("  T, Tail, Tails");
+    println!("*Cling* - The coin flips into the air!");
     println!(" ");
 
-    let mut guess = String::new();
-    io::stdin()
-        .read_line(&mut guess)
-        .expect("Failed to read line");
-
-    let guessed_coin_face = match guess.trim() {
-        "H" => Some(CoinFace::Heads),
-        "Head" => Some(CoinFace::Heads),
-        "Heads" => Some(CoinFace::Heads),
-        "T" => Some(CoinFace::Tails),
-        "Tail" => Some(CoinFace::Tails),
-        "Tails" => Some(CoinFace::Tails),
-        _ => None,
+    let tossed_coin_face = match rand::random::<bool>() {
+        true => CoinFace::Heads,
+        false => CoinFace::Tails,
     };
 
-    if guessed_coin_face.is_some() {
-        if guessed_coin_face.unwrap() == game.tossed_coin_side {
-            println!("You guessed correct!");
+    loop {
+        println!("Guess a side!");
+        println!("Enter one of the following options:");
+        println!(" ");
+        println!("For Heads:");
+        println!("  H, Head, Heads");
+        println!(" ");
+        println!("For Tails:");
+        println!("  T, Tail, Tails");
+        println!(" ");
+
+        let mut guess = String::new();
+        io::stdin()
+            .read_line(&mut guess)
+            .expect("Failed to read line");
+
+        let guessed_coin_face = match guess.trim() {
+            "H" => Some(CoinFace::Heads),
+            "Head" => Some(CoinFace::Heads),
+            "Heads" => Some(CoinFace::Heads),
+            "T" => Some(CoinFace::Tails),
+            "Tail" => Some(CoinFace::Tails),
+            "Tails" => Some(CoinFace::Tails),
+            _ => None,
+        };
+
+        if guessed_coin_face.is_some() {
+            let guessed_coin_face = guessed_coin_face.unwrap();
+            println!(" ");
+            println!("*Cling* - The coin lands on the ground");
+            println!("Coin face showing: {:?}", guessed_coin_face);
+            println!(" ");
+
+            if guessed_coin_face == tossed_coin_face {
+                println!("You guessed correct!");
+                break;
+            } else {
+                println!("You lost.");
+                break;
+            }
         } else {
-            println!("Wrong, try again.")
+            println!("Invalid input, try again!");
         }
     }
 }
